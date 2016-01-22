1a0ee3499e7328cbe8827f193fe68f6d9959e706
santiaago <santiago.arias@red-gate.com>
2016-01-22 11:52:27 +0000

    softmax solution

--- softmax.py
+++ softmax.py
@@ -6,7 +6,8 @@ import numpy as np
 
 def softmax(x):
     """Compute softmax values for each sets of scores in x."""
-    pass  # TODO: Compute and return softmax(x)
+
+    return np.exp(x) / np.sum(np.exp(x), axis = 0)
 
 print(softmax(scores))
 
